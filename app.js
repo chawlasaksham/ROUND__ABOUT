2f22bec62c104929524edeb168025f95fc63dbb8
chawlasaksham <chawlasaksham02@gmail.com>
2025-01-23 13:36:26 +0530

    Prepare for Render deployment

--- app.js
+++ app.js
@@ -115,6 +115,7 @@ app.use((err, req, res, next) => {
     res.status(statuscode).json({ message, details: err.details });
 });
 
-app.listen(8080, () => {
-    console.log("erver is listening on port 8080");
-   });
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
